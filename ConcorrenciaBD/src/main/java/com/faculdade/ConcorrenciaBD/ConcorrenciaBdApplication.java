d498ee7f904be876affac7a6d8d9945c1cd0f31b
henrique.souza <henrique.souza@cinbesa.com.br>
2026-06-07 15:04:43 -0300

    fix: renomear arquivos

--- ConcorrenciaBD/src/main/java/com/faculdade/ConcorrenciaBD/ConcorrenciaBdApplication.java
+++ ConcorrenciaBD/src/main/java/com/faculdade/ConcorrenciaBD/ConcorrenciaBdApplication.java
@@ -1,13 +1,13 @@
-package com.faculdade.demo;
+package com.faculdade.ConcorrenciaBD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ConcorrenciaBdApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ConcorrenciaBdApplication.class, args);
 	}
 
 }
--- ConcorrenciaBD/src/test/java/com/faculdade/ConcorrenciaBD/ConcorrenciaBdApplicationTests.java
+++ ConcorrenciaBD/src/test/java/com/faculdade/ConcorrenciaBD/ConcorrenciaBdApplicationTests.java
@@ -1,10 +1,10 @@
-package com.faculdade.demo;
+package com.faculdade.ConcorrenciaBD;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ConcorrenciaBdApplicationTests {
 
 	@Test
 	void contextLoads() {
