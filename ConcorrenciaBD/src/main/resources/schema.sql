d964f85494042361ff354e75d1b37887f83ad690
henrique souza <henrique.souza@cinbesa.com.br>
2026-06-08 11:10:28 -0300

    fix: corrigir swagger

--- ConcorrenciaBD/src/main/resources/schema.sql
+++ ConcorrenciaBD/src/main/resources/schema.sql
@@ -1,12 +1,14 @@
 create table contas_bancarias (
     id serial primary key,
     numero_conta varchar(255),
-    saldo NUMERIC(15, 2)
+    saldo NUMERIC(15, 2),
+    titular varchar(255)
 );
 
 create table contas_bancarias_versionada (
     id serial primary key,
     numero_conta varchar(255),
     saldo NUMERIC(15, 2),
-    version integer
+    version integer,
+    titular varchar(255)
 );
